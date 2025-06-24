f49ff75edcd6da873cfeaab02937b61139addfdc
Tom Hunze <dev@thunze.de>
2025-06-24 21:25:16 +0200

    Add reproduction example

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,19 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
+<script lang="ts">
+  import { buttonVariants } from "$lib/components/ui/button/index.js";
+  import * as Tooltip from "$lib/components/ui/tooltip/index.js";
+
+  const sides: ("top" | "bottom" | "left" | "right")[] = ["top", "bottom", "left", "right"];
+</script>
+
+<div class="flex flex-wrap gap-8">
+    {#each sides as side}
+        <Tooltip.Provider>
+            <Tooltip.Root>
+                <Tooltip.Trigger class={buttonVariants({ variant: "outline" })}>{side}</Tooltip.Trigger>
+                <Tooltip.Content side={side}>
+                    <p>Add to library</p>
+                </Tooltip.Content>
+            </Tooltip.Root>
+        </Tooltip.Provider>
+    {/each}
+</div>
